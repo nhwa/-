7d11e363a9cdc12f4c5eb424305c0b03e5de69c1
seadonggyun4 <seadonggyun@gmail.com>
2022-04-30 21:02:53 +0900

    🚀 동균 14681 문제풀이

---
+++ 2step/14681/14681_dgseo.js
@@ -0,0 +1,41 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+let input = []
+
+
+rl.on("line", (line) => {
+  input.push(line)
+
+  const num1 = Number(input[0])
+  const num2 = Number(input[1])
+
+  if(num1 > 0 && num2 > 0){
+    console.log(1)
+  } else if(num1 > 0 && num2 < 0){
+    console.log(4)
+  } else if(num1 < 0 && num2 > 0){
+    console.log(2)
+  } else if(num1 < 0 && num2 < 0){
+    console.log(3)
+  }
+
+
+})
+
+
+rl.on('close', () => {
+  process.exit();
+})
+
+
+
+
+
+
+
+
