1474450188b975b5c27b9ddb72f3ddadd9e0d460
nhwa <44343908+nhwa@users.noreply.github.com>
2022-05-02 16:18:24 +0900

    2439_nhwa

---
+++ 3step/2439/2439_nhwa.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class q_2439 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N-1-i; j++)
+                bw.write(" ");
+            for(int j=0; j<=i; j++)
+                bw.write("*");
+            bw.newLine();
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+
+    }
+}
